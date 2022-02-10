5db0f2378f1fee92b0b4e4148b60563a96485914
nabil <merchant.nabil@gmail.com>
2022-02-10 10:34:37 -0600

    Adding selected for when buttons clicked

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -38,7 +38,7 @@ class Movies extends Component {
   };
 
   handleGenreSelect = (genre) => {
-    console.log(genre);
+    this.setState({ selectedGenre: genre });
   };
 
   render() {
@@ -54,6 +54,7 @@ class Movies extends Component {
         <div className="col-2">
           <ListGroup
             items={this.state.genres}
+            selectedItem={this.state.selectedGenre}
             onItemSelect={this.handleGenreSelect}
           />
         </div>
