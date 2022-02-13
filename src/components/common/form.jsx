4cb2539fd3157752fc2ad4d3182058cc780f0a60
Nabil Merchant <nabim@amazon.com>
2022-02-13 00:05:45 -0600

    Adding a form to add movies

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Joi } from 'joi-browser';
+import Joi from 'joi-browser';
+import Input from './input';
+import Select from './select';
 
 class Form extends Component {
   state = {
@@ -13,9 +15,7 @@ class Form extends Component {
     if (!error) return null;
 
     const errors = {};
-    for (let item of error.details) {
-      errors[item.path[0]] = item.message;
-    }
+    for (let item of error.details) errors[item.path[0]] = item.message;
     return errors;
   };
 
@@ -23,16 +23,13 @@ class Form extends Component {
     const obj = { [name]: value };
     const schema = { [name]: this.schema[name] };
     const { error } = Joi.validate(obj, schema);
-
     return error ? error.details[0].message : null;
   };
 
-  handleSubmit = (event) => {
-    // Prevents the app from making a network call, which is the default for HTML forms.
-    event.preventDefault();
+  handleSubmit = (e) => {
+    e.preventDefault();
 
     const errors = this.validate();
-    // console.log(errors);
     this.setState({ errors: errors || {} });
     if (errors) return;
 
@@ -47,8 +44,47 @@ class Form extends Component {
 
     const data = { ...this.state.data };
     data[input.name] = input.value;
+
     this.setState({ data, errors });
   };
+
+  renderButton(label) {
+    return (
+      <button disabled={this.validate()} className="btn btn-primary">
+        {label}
+      </button>
+    );
+  }
+
+  renderSelect(name, label, options) {
+    const { data, errors } = this.state;
+
+    return (
+      <Select
+        name={name}
+        value={data[name]}
+        label={label}
+        options={options}
+        onChange={this.handleChange}
+        error={errors[name]}
+      />
+    );
+  }
+
+  renderInput(name, label, type = 'text') {
+    const { data, errors } = this.state;
+
+    return (
+      <Input
+        type={type}
+        name={name}
+        value={data[name]}
+        label={label}
+        onChange={this.handleChange}
+        error={errors[name]}
+      />
+    );
+  }
 }
 
 export default Form;
