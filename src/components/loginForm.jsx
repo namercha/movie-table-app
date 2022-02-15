c7d967c622470be7260b27741402c132413c2789
nabil <merchant.nabil@gmail.com>
2022-02-15 17:06:11 -0600

    Ensuring only admins can see the movie delete column

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Joi from 'joi-browser';
 import Form from './common/form';
 import auth from '../services/authService';
+import { Redirect } from 'react-router-dom';
 
 class LoginForm extends Form {
   state = {
@@ -19,7 +20,8 @@ class LoginForm extends Form {
     try {
       const { data } = this.state;
       await auth.login(data.username, data.password);
-      window.location = '/'; // Will cause a full reload of the web page
+      const { state } = this.props.location;
+      window.location = state ? state.from.pathname : '/'; // Will cause a full reload of the web page
     } catch (ex) {
       if (ex.response && ex.response.status === 400) {
         const errors = { ...this.state.errors };
@@ -30,6 +32,8 @@ class LoginForm extends Form {
   };
 
   render() {
+    if (auth.getCurrentUser()) return <Redirect to="/" />;
+
     return (
       <div>
         <h1>Login</h1>
