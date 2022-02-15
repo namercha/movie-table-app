155521e3f17d36c24442f6afe2094b78ef9ac6f3
nabil <merchant.nabil@gmail.com>
2022-02-15 05:39:07 -0600

    Refactoring

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -3,10 +3,28 @@ import { apiUrl } from '../config.json';
 
 const apiEndpoint = apiUrl + '/movies';
 
+function movieUrl(id) {
+    return `${apiEndpoint}/${id}`;
+}
+
 export function getMovies() {
     return httpService.get(apiEndpoint);
 }
 
+export function getMovie(movieId) {
+    return httpService.get(movieUrl(movieId));
+}
+
+export function saveMovie(movie) {
+    if (movieId._id) {
+        const body = {...movie };
+        delete body_.id;
+        return httpService.put(movieUrl(movie._id), body);
+    }
+
+    return httpService.post(apiEndpoint, movie);
+}
+
 export function deleteMovie(movieId) {
-    return httpService.delete(apiEndpoint + '/' + movieId);
+    return httpService.delete(movieUrl(movieId));
 }
